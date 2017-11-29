7323fcdc347f04b043b52ba86aaf3b5326193f63
wuheng <admin@80boys.com>
2017-11-29 15:35:28 +0800

    222222333

--- aliyun-php-sdk-core/RoaAcsRequest.php
+++ aliyun-php-sdk-core/RoaAcsRequest.php
@@ -17,6 +17,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+namespace  Aliyun\Core; 
 abstract class RoaAcsRequest extends AcsRequest
 {
     protected $uriPattern;
